9e6f180464708cf7ab1da1714af310f3beb60468
HectorGinory <hectorginorydevs@gmail.com>
2023-03-08 12:18:31 +0100

    Started with the powerOn styles

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 function powerBtn() {
     let powerBtn = document.getElementById('power-btn');
     let gameBoy = document.getElementById('gameboy')
@@ -6,12 +5,18 @@ function powerBtn() {
     if (document.getElementsByClassName('btn-off').length == 1) {
         powerBtn.classList.remove('btn-off');
         powerBtn.classList.add('btn-on');
-        gameboy.classList.remove('btn-off');
-        gameboy.classList.add('btn-on');
+        gameboy.classList.remove('gameboy-container');
+        gameboy.classList.add('gameboy-containerOn');
     } else {
         powerBtn.classList.remove('btn-on');
         powerBtn.classList.add('btn-off');
-        gameboy.classList.remove('btn-on');
-        gameboy.classList.add('btn-off');
+        gameboy.classList.remove('gameboy-containerOn');
+        gameboy.classList.add('gameboy-container');
     }
+}
+
+let screenVideo() {
+    let screen = document.getElementById('screen')
+
+    let nintendoLogo
 }
